ab409ed373b4595b542b145177b3ebcbf00e622f
wcy <792053282@qq.com>
2019-10-10 20:50:58 +0800

    前端页面push

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,54 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import productShow from '@/components/productShow'
+import product from '@/components/product'
+import touzi from '@/components/touzi'
+import touziShow from '@/components/touziShow'
+import allproduct from '@/components/allproduct'
+import welcome from '@/components/welcome'
+import myself from '@/components/myself'
+import Test from '@/components/Test'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+      path: '/',
+      name: 'welcome',
+      component: welcome
+    },{
+      path: '/allproduct',
+      name: 'allproduct',
+      component: allproduct
+    },{
+      path: '/productShow/:status',
+      name: 'productShow',
+      component: productShow
+    },  {
+      path:'/touzi/:pid',
+      name:'/touzi',
+      component:touzi
+    },{
+      path:'/product/:pid',
+      name:'product',
+      component:product
+    },{
+      path:'/touziShow/:status',
+      name:'touziShow',
+      component:touziShow
+    },{
+      path:'/myself',
+      name:'myself',
+      component:myself
+    },{
+      path:'/Test',
+      name:'Test',
+      component:Test
     }
   ]
 })
