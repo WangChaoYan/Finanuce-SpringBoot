e5b04b5c1b99d17d37c576c3ecc0cf49a0008720
hxh <1607706432@qq.com>
2019-10-11 08:50:13 +0800

    前后台登陆注册

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,42 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '/HelloWorld',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },{
+      path: '/',
+      name: 'welcome',
+      component: welcome
+    },{
+      path: '/allproduct',
+      name: 'allproduct',
+      component: allproduct
+    },{
+      path: '/productShow/:status',
+      name: 'productShow',
+      component: productShow
+    },  {
+      path:'/touzi/:pid',
+      name:'/touzi',
+      component:touzi
+    },{
+      path:'/product/:pid',
+      name:'product',
+      component:product
+    },{
+      path:'/touziShow/:status',
+      name:'touziShow',
+      component:touziShow
+    },{
+      path:'/myself',
+      name:'myself',
+      component:myself
+    },{
+      path:'/Test',
+      name:'Test',
+      component:Test
+    },{
       path: '/',
       name: 'login',
       component: login
